42d8d3da8694e1d7c36f83cdc01bdb8a7e28aa4a
Raihan S <raihan.act@gmail.com>
2025-08-30 17:15:15 +0600

    .

--- app/Mainframe/Modules/Users/Traits/UserTrait.php
+++ app/Mainframe/Modules/Users/Traits/UserTrait.php
@@ -841,4 +841,14 @@ trait UserTrait
         $notification->process()->save();
     }
 
+    /**
+     * Retrieve the timezone.
+     *
+     * @return string
+     */
+    public function timezone()
+    {
+        return $this->timezone ?? config('app.timezone');
+    }
+
 }
